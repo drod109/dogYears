cb0eaffe4b080837b477a218d11a91a8f5319fb1
Dwayne N Rodrigues <drodrigues@360incentives.com>
2017-10-30 20:37:05 -0400

    Create dogYears.js

---
+++ dogYears.js
@@ -0,0 +1,23 @@
+// Set variable and assign number
+let myAge = 42;
+
+// Set variable and assign number
+let earlyYears = 2;
+
+// Apply multiplication assignment to variable
+earlyYears *= 10.5;
+
+// Apply subtraction assignment to variable
+let laterYears = myAge -= 2;
+
+// Applying multiplication assignment variable
+laterYears *= 4;
+
+// Set variable and assign results
+let myAgeInDogYears = earlyYears += laterYears
+
+// Set and assign variable
+let myName = 'Dwayne'.toLowerCase();
+
+// Console results
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
